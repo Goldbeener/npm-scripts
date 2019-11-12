c23df1710bd31f0c8052730e894fba492ee284ce
duxin <duxin@kuaishou.com>
2019-11-12 10:45:16 +0800

    feat: build

---
+++ client/scripts/main.js
@@ -0,0 +1,6 @@
+const man = {
+    name: 'Sean',
+    age: '18'
+};
+const { name } = man;
+console.log('LiveReload Demo', name);
---
+++ dist/scripts/main.6447b20f.js
@@ -0,0 +1,1 @@
+const man={name:"Sean",age:"18"};const{name:name}=man;console.log("LiveReload Demo",name);

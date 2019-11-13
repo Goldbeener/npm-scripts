8301f8fc394f56c1f16235b84a0cf68bdad3ec02
duxin <duxin@kuaishou.com>
2019-11-13 09:55:48 +0800

    feat: add service

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.static('./dist'));
+app.use(morgan('combined'));
+
+app.listen(port, err => {
+    if (err) {
+        console.error('server start err');
+        process.exit(1);
+    }
+    console.log(`server started at port ${port}`);
+});
